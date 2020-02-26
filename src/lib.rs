4e3ef4e7bc9ee90be913e3471f666e3c1ddd7630
Benjamin Schubert <bschubert@ca.ibm.com>
2020-02-26 16:26:45 -0500

    Get default log path

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,22 @@
+extern crate dirs;
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_returns_sol() {
-        assert_eq!(get_star_system(), String::from("Sol"));
+        assert_eq!(get_star_system().unwrap(), String::from("Sol"));
     }
 }
 
-pub fn get_star_system() -> String {
-    String::from("Sol")
+pub fn get_star_system_from_path(path: &std::path::Path) -> Option<String> {
+    Some(String::from("Sol"))
+}
+
+pub fn get_star_system() -> Option<String> {
+    let mut log_dir: std::path::PathBuf = dirs::home_dir()?;
+    log_dir.push(r"Saved Games/Frontier Developments/Elite Dangerous");
+
+    get_star_system_from_path(log_dir.as_path())
 }
