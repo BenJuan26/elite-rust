c89464cbebd1ee27678decbb65dc94536122f26e
Benjamin Schubert <bschubert@ca.ibm.com>
2020-03-03 10:10:59 -0500

    Change name to simply elite

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,7 +3,7 @@ extern crate lazy_static;
 
 use std::path::PathBuf;
 
-use elite_rust;
+use elite;
 
 lazy_static! {
     static ref DATA_DIR: PathBuf = get_data_dir();
@@ -20,12 +20,12 @@ fn get_data_dir() -> PathBuf {
 #[test]
 fn it_returns_star_system() {
     let path = DATA_DIR.clone();
-    assert_eq!(elite_rust::get_star_system_from_path(path.as_path()).unwrap(), String::from("Sol"));
+    assert_eq!(elite::get_star_system_from_path(path.as_path()).unwrap(), String::from("Sol"));
 }
 
 #[test]
 fn it_returns_status() {
-    let status = elite_rust::get_status_from_path(DATA_DIR.as_path()).unwrap();
+    let status = elite::get_status_from_path(DATA_DIR.as_path()).unwrap();
     assert_eq!(status.raw_flags, 18939917);
     assert_eq!(status.pips[1], 8);
     assert_eq!(status.timestamp, String::from("2020-03-03T05:03:21Z"));
